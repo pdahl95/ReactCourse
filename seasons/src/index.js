d9d5485099b9d02dbc64f2fe3a11604b88e9730c
Pernille Dahl <pdahl@csumb.edu>
2020-10-02 19:48:21 -0700

    added logic for summer/winter

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner.js';
 
 class App extends React.Component {
-    constructor(props){
-        super(props);
-        // THIS IS THE ONLY TIME WE DIRECT ASSIGNMENT
-        this.state = { lat: null }; // state object
-
+    state = { lat: null, errorMessage:''};
+    
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-            // we called setState()
-                this.setState({ lat: position.coords.latitude });
-            }, // positive callback!
-            (error) => console.log(error)
+            (position) => this.setState({ lat: position.coords.latitude }), 
+            (error) => this.setState({ errorMessage: error.message })
         );
-
     }
 
-    // React says we have to define render!!!!
+    renderContent(){
+        if(this.state.errorMessage && !this.state.lat){
+            return <div> Error: {this.state.errorMessage}</div>
+        }
+        if(!this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+        return <Spinner message="Please accept location request"/>
+    }
+
     render(){
-        return <div> Latitude: {this.state.lat} </div>;
+        return <div className="border red">
+            {this.renderContent()}
+        </div>
     }
 }
-
-
 
 ReactDOM.render(<App/>, document.querySelector("#root"));
